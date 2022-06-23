9dfa07dd92c6a5c3fc268eb1c0f935b63107996f
Squbble <41349022+Squbble@users.noreply.github.com>
2022-06-23 21:27:09 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+	if (n < 0) {
+		let a = n * (-1);
+		return Number(a.toString().split('').reverse().join(''));
+	} else {
+		return Number(n.toString().split('').reverse().join(''));
+	}
 }
